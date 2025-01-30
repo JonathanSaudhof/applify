4c8d8c8b7f3a293b4d3e1037c61d5e6987cb49fd
Jonathan Saudhof <jonathan.saudhof@adesso.de>
2025-01-30 16:32:30 +0100

    chore: refactoring layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,13 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 
+import { Button } from "@/components/ui/button";
+import { invalidateApplicationsList } from "@/feature/application/components/actions/revalidation";
+import CreateApplication from "@/feature/application/components/create-application";
 import { Menu } from "@/feature/settings/components/menu";
 import { TRPCReactProvider } from "@/trpc/react";
-import { api, HydrateClient } from "@/trpc/server";
+import { HydrateClient } from "@/trpc/server";
+import { RefreshCw } from "lucide-react";
 import Link from "next/link";
 
 export const metadata: Metadata = {
@@ -18,7 +22,10 @@ export const metadata: Metadata = {
 export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const template = await api.config.getTemplateFile();
+  const handleRefresh = async () => {
+    "use server";
+    await invalidateApplicationsList();
+  };
 
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
@@ -33,7 +40,13 @@ export default async function RootLayout({
                     <h1 className="text-2xl font-semibold">Applify</h1>
                   </div>
                 </Link>
-                <Menu />
+                <div className="flex items-center gap-4">
+                  <Button variant="outline" onClick={handleRefresh}>
+                    <RefreshCw />
+                  </Button>
+                  <CreateApplication />
+                  <Menu />
+                </div>
               </header>
               {children}
             </HydrateClient>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,15 @@
-import { Button } from "@/components/ui/button";
-import { invalidateApplicationsList } from "@/feature/application/components/actions/revalidation";
 import ApplicationsList, {
   ApplicationListSkeleton,
 } from "@/feature/application/components/application-list";
-import CreateApplication from "@/feature/application/components/create-application";
 import { api, HydrateClient } from "@/trpc/server";
-import { RefreshCw } from "lucide-react";
 import { Suspense } from "react";
 
 export default async function Home() {
   const config = await api.config.getConfigFile();
 
-  const handleRefresh = async () => {
-    "use server";
-    await invalidateApplicationsList();
-  };
-
   return (
     <HydrateClient>
       <main className="">
-        <section className="flex justify-between border-b-2 p-8">
-          <div className="flex gap-4">
-            <Button variant="outline" onClick={handleRefresh}>
-              <RefreshCw />
-            </Button>
-            <CreateApplication />
-          </div>
-        </section>
         {config?.baseFolder ? (
           <Suspense fallback={<ApplicationListSkeleton />} key={Math.random()}>
             <ApplicationsList folderId={config.baseFolder.id} />
